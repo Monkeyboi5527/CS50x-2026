52fe7793ff5ab7216287a9ed8634cdba95c0f884
MonkeyBoi <monkeylaguri308@gmail.com>
2026-05-26 22:40:55 -0400

    hello.c & Lecture 1 Notes

---
+++ Week 1 - C/hello.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// I am using CLion
+
+// Specific for me
+// cd "folder name"
+
+// In terminal Command Line Interface (CLI)
+// $ code hello.c
+// creates new C file
+
+// Graphical User Interface
+int main(void)
+{
+    printf("hello, world\n");
+    // To compile
+    // $ make hello
+    // creates a new app called hell.exe
+    // Compiles Source Code into Machine Code 
+    
+    // Specific for me
+    // Use the run button/function
+    
+    // To run
+    // $ ./hello
+    // Since the program is somewhere in the folder it finds and runs it
+}
