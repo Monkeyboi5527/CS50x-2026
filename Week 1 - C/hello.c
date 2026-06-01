5ab1f91acff9fb99b18f2643c98907a3a8040060
MonkeyBoi <monkeylaguri308@gmail.com>
2026-06-01 13:19:56 -0400

    hello.c changes

--- Week 1 - C/hello.c
+++ Week 1 - C/hello.c
@@ -4,6 +4,7 @@
 // A library
 
 // manual.cs50.io -  CS50’s C documentation
+ #include <cs50.h>
 
 // I am using CLion
 
@@ -43,5 +44,41 @@ int main(void)
       | ~                            
 
      */
-    // In line 15 there is a missing semi colon
+    // In line 15 there is a missing semicolon
+    
+    // To make cs50.h I had to use WSL and Ubuntu
+    // To use cs50.h on Windows with CLion:
+    // 1. Enable WSL (Windows Subsystem for Linux) and install Ubuntu
+    // 2. In Ubuntu terminal, install build tools:
+    //    sudo apt-get install cmake gcc g++ gdb build-essential
+    // 3. Install cs50 library from source:
+    //    wget https://github.com/cs50/libcs50/archive/refs/heads/main.zip
+    //    sudo apt install unzip && unzip main.zip
+    //    cd libcs50-main && sudo make install
+    // 4. In CLion, add WSL as the toolchain (Settings -> Toolchains -> + -> WSL)
+    // 5. In CMakeLists.txt add:
+    //    include_directories(/usr/local/include)
+    //    link_directories(/usr/local/lib)
+    //    target_link_libraries(your_project cs50)
+    // 6. To compile manually in Ubuntu terminal:
+    //    cc file.c -lcs50 -L/usr/local/lib -o hello && ./hello
+    
+    // Scratch
+    // say "What's your name?" and wait
+    // Oval Answer variable
+    
+    // C
+    string answer = get_string("What's your name?\n");
+    printf("hello, %s\n", answer);
+    // %s us the placeholder for a string
+    // the value for the placeholder is outside the quotes.
+    
+    // Linux Terminal Commands:
+    // cd folder_name   - Change directory (navigate into a folder)
+    // cp file dest     - Copy a file to a destination
+    // ls               - List files and folders in current directory
+    // mkdir name       - Make a new directory (folder)
+    // mv file dest     - Move (or rename) a file
+    // rm file          - Remove (delete) a file
+    // rmdir folder     - Remove an empty directory
 }
