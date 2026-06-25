a8ec919410fe35a05ea1ad6f2adc6139ae1db4a1
MonkeyBoi <monkeylaguri308@gmail.com>
2026-06-24 22:59:01 -0400

    Data types

--- Week 1 - C/compare.c
+++ Week 1 - C/compare.c
@@ -20,6 +20,9 @@ int main(void)
     {
         printf("%d == %d\n", x, y);
     }
+    // else if (x == y) works the same but is less efficient
+    
+    
     /*
      * =  assignment operator
      * == equals
@@ -29,4 +32,13 @@ int main(void)
      * >= greater than or equal to
      * != not equal
      */
+    
+    // Data Types
+    bool hi = false; // A Yes or No statement, 0 or 1
+    char a = 'a'; // A single character
+    double d = 3.1400000000000000000000000000000000000; // float with 64 bits
+    float f = 3.14; // "floating point" 32 bits, a number with a decimal
+    int i = 42; // 32 bits, simple integers 
+    long l = 4200000000l; // 64 bits, simple integers
+    string s = "Hello World!"; // Multiple characters in a line
 }
