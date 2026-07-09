2700e9a6f60bc4a7deb6b43541b3e897afa8c216
MonkeyBoi <monkeylaguri308@gmail.com>
2026-07-08 23:24:18 -0400

    Update cat.c

--- Week 1 - C/cat.c
+++ Week 1 - C/cat.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-
+#include <cs50.h>
 
 int main (void)
 {
-    for (int i = 0; i <= 3; i++)
-    {
-        printf("meow\n");
-    }
+   int n = get_int("How many "); // Has a built in try catch
+   
+   for (int i = 0; i < n; i++)
+   {
+      printf("meow\n");
+   }
 }
