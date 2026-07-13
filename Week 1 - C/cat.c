e3ec17c6d2153af0cf0705dd73eccc085b12b58b
MonkeyBoi <monkeylaguri308@gmail.com>
2026-07-12 23:42:07 -0400

    cat.c line comments

--- Week 1 - C/cat.c
+++ Week 1 - C/cat.c
@@ -3,15 +3,15 @@
 
 int main (void)
 {
-   int n;
+   int n; //Creates an int n but does not assign it
    do
    {
-      n = get_int("How many ");
+      n = get_int("How many "); // Asks until the number is positive
    }
-   while (n < 0);
+   while (n < 0); // repeat if input is negative
    
-   for (int i = 0; i < n; i++)
+   for (int i = 0; i < n; i++) // loop n times
    {
-      printf("meow\n");
+      printf("meow\n"); // print "meow" once per iteration
    }
 }
