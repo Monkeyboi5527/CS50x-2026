20dcf519f3e1ecc3d66b33bfcc38ce03072faeff
MonkeyBoi <monkeylaguri308@gmail.com>
2026-07-13 17:55:21 -0400

    cat.c meow function

--- Week 1 - C/cat.c
+++ Week 1 - C/cat.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <cs50.h>
 
+   // In C and C++ the convention is to declare the function before the main()
+   
+// The prototype
+void meow(void);
+void meow2(int n);
+
 int main (void)
 {
+   /*
    int n; //Creates an int n but does not assign it
    do
    {
@@ -14,4 +21,31 @@ int main (void)
    {
       printf("meow\n"); // print "meow" once per iteration
    }
+   */
+   
+   /*
+   for (int i = 0; i < 3; i++)
+   {
+      meow();
+   }
+   */
+   
+   int n = get_int("How many times");
+   meow2(n); // n in the function is not in the scope of the main 
+}
+
+   // Initializes the function here
+
+// creates a function named meow 
+void meow(void) // void -> doesn't return anything | doesn't take in an input
+{
+      printf("meow\n"); // this is just a byproduct of running meow
+}
+
+void meow2(int n) // has an input of n
+{
+      for (int i = 0; i < n; i++) // loop n times
+      {
+         printf("meow\n"); 
+      }
 }
