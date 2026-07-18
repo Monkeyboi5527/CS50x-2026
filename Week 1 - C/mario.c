cd084933bb3810e5c65613abc5802ceda52c4667
MonkeyBoi <monkeylaguri308@gmail.com>
2026-07-17 23:37:00 -0400

    rows and columns

--- Week 1 - C/mario.c
+++ Week 1 - C/mario.c
@@ -1,6 +1,32 @@
+#include <cs50.h>
 #include <stdio.h>
 
+void rows (int);
+void columns (int);
+void box (void);
+
 int main(void)
+{
+    rows (get_int("How many rows"));
+}
+
+    void rows (int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        printf("?");
+    }
+}
+
+    void columns (int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        printf("?\n");
+    }
+}
+
+    void box (void)
 {
     
 }
